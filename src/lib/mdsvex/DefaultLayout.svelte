ad70e51b285620c55c18864c253db3af157bbfea
al <sanjari.alfian@nds.co.id>
2026-04-02 00:35:20 +0700

    rallback

--- src/lib/mdsvex/DefaultLayout.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  export let title = '';
-  export let description = '';
-</script>
-
-<div class="max-w-4xl mx-auto px-6 py-12">
-  <slot />
-</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,2 +1,26 @@
-/** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
-export default {}
+import adapter from '@sveltejs/adapter-static';
+import { mdsvex } from 'mdsvex';
+import remarkGfm from 'remark-gfm';
+import remarkSmartypants from 'remark-smartypants';
+
+const mdsvexConfig = {
+  extensions: ['.svx', '.md'],
+  remarkPlugins: [remarkGfm, remarkSmartypants],
+  rehypePlugins: [],
+};
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  extensions: ['.svelte', ...mdsvexConfig.extensions],
+  preprocess: [mdsvex(mdsvexConfig)],
+  kit: {
+    adapter: adapter({
+      fallback: 'index.html',
+    }),
+    paths: {
+      base: '',
+    },
+  },
+};
+
+export default config;
